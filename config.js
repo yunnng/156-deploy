1d312251821e7a4cf1bb8a1e8ee02d6adbe16a0f
yang <hengyang@shein.com>
2020-03-08 14:25:25 +0800

    dev: dev

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
   repositoriesPath: [
+    // '../156/156-deploy',
     '../156/app_h5',
   ],
 }
--- service/repositoriesList.js
+++ service/repositoriesList.js
@@ -10,10 +10,10 @@ const getMostRecentCommit = (repository) => repository.getBranchCommit('master')
 const getCommitMessage = (commit) => commit.message()
 
 config.repositoriesPath.forEach(async (path) => {
-  console.log(path)
 
   const pathToRepo = require('path').resolve(path)
   const repo = await git.Repository.open(pathToRepo)
+  console.log(repo)
 
   // branch list
   // repo.getReferenceNames(3)
@@ -27,9 +27,9 @@ config.repositoriesPath.forEach(async (path) => {
   //   .then((reference) => {
   //     // checkout branch
   //     const options = new CheckoutOptions()
-  //     return repo.checkoutBranch('act-flash-sale')
+  //     return repo.checkoutRef(reference)
+  //     // return repo.checkoutBranch('act-flash-sale', options) // 仅本地已存在的分支
   //   })
-  //   .then(msg => console.log(1, msg))
 
   // repo.getBranchCommit('master')
   //   .then((commit) => {
@@ -41,12 +41,39 @@ config.repositoriesPath.forEach(async (path) => {
   //   })
 
   // commit list
-  const walker = git.Revwalk.create(repo)
-  // 通过pushGlob来获取所有分支的提交记录
-  walker.pushGlob('*')
-  // 获取符合日期的commits
-  const commits = await walker.getCommitsUntil(() => true)
-  commits.forEach(commit => console.log(`${commit.sha().substr(0, 8)} [${commit.date().toLocaleString()}] ${commit.author().name()} - ${commit.message().trim()}`))
+  // const walker = git.Revwalk.create(repo)
+  // // 通过pushGlob来获取所有分支的提交记录
+  // walker.pushGlob('*')
+  // // 获取符合日期的commits
+  // const commits = await walker.getCommitsUntil(() => true)
+  // commits.forEach(commit => console.log(`${commit.sha().substr(0, 8)} [${commit.date().toLocaleString()}] ${commit.author().name()} - ${commit.message().trim()}`))
+
+  repo.getBranch('refs/remotes/origin/master')
+    .then((ref) => {
+      Branch.name(ref).then((string) => {
+        // Use string
+        console.log(string)
+        return repo.fetch(string)
+      })
+        .then((a) => {
+          console.log(a)
+        }, (b) => {
+          console.log(b)
+        })
+    })
+  // repo.mergeBranches('master', 'origin/master') // 仅本地已存在的分支
+  // repo.getBranch('refs/remotes/origin/master')
+  //   .then((ref) => {
+  //     for (var a in ref.repo) {
+  //       console.log(a)
+  //     }
+  //     // console.log(ref.isBranch())
+  //     return repo.fetch(ref)
+  //   }).then((msg) => {
+  //   console.log(msg)
+  // }, (msg) => {
+  //   console.log(msg)
+  // })
   // console.log(commits)
   // repo.then(getMostRecentCommit(repo))
   //   .then(getCommitMessage)
